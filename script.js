103ce48a5aca8e2ba288905b39c0946e889d6c14
Risi Ferraz <risiferraz@gmail.com>
2022-07-22 07:13:52 -0300

    Ajustes diversos

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
 //*******CRONOMETRO********
-const cronometro =new Cronometro()
-    setInterval(() => {
-    cronometro.atualizaCronometro()
-    }, 1000);
-    //*******FUNÇÕES QUE ACONTECEM AO CLICAR NO "START"*******/
-    function clicar() {
-      document.getElementById("tela-inicial").style.display = "none"
-      document.getElementById("jogo-em-andamento").style.display = "block"
-      cronometro.iniciaCronometro()
-    }
-//*******FUNÇÕES QUE ACONTECEM AO DRAGAR*******/
-const cards = document.querySelectorAll('.card')//Chamar (procurar) todos os elementos que tem a classe cards do HTML
-const dropzones = document.getElementsByClassName('container')
-
-cards.forEach(card => {//para cada um dos cards achados no HTML crie um "card"
-    card.addEventListener('dragstart', dragStart) // adicione um evento que será dispara assim que o usuário começa a arrastar um item - É aqui que podemos definir os (drag data) dados de arrasto - dragstart é um EVENTO', e dragStart é uma função
-    // card.addEventListener('drag', Drag) // Dispara quando um item "dragável" (elemento ou texto) é arrastado
-    // card.addEventListener('dragend', dragEnd) // É acionado quando uma operação "DRAG" termina (como soltar o botão do mouse ou apertar a tecla Esc) - Após terminado o evento "dragend", a operação arrastar e soltar está completa
-})
-
-function dragStart() {
-  dropzones.forEach(dropzone => dropzone.classList.add('highlight')) //(39:27/42:21)
-  //"this" refere-se ao "card" => vou adicionar ao elemento "card" uma classe que vou chamar de "highlight"
-  // this.classList.add('is-dragging')//(45:35/47:23/52:03)
+const cronometro = new Cronometro()
+setInterval(() => {
+  cronometro.atualizaCronometro()
+}, 1000);
+//*******FUNÇÕES QUE ACONTECEM AO INICIAR*******/
+function passar() {
+  document.getElementById("capa").style.display = "none"
+  document.getElementById("tela-inicial").style.display = "block"
 }
+//*******FUNÇÕES QUE ACONTECEM AO CLICAR NO "START"*******/
+function clicar() {
+  document.getElementById("tela-inicial").style.display = "none"
+  document.getElementById("jogo-em-andamento").style.display = "grid"
+  cronometro.iniciaCronometro()
+  card.embaralhaCards()
+}
+//*******FUNÇÕES QUE ACONTECEM NO DRAG AND DROP"*******/
+let dragged = null;
 
-// dropzones.forEach(dropzone => {
-//   dropzone.addEventListener('dragenter', dragEnter) // Dispara quando um item arrastado entra em uma div (alvo de queda) válido (dropável)
-//   dropzone.addEventListener('dragover', dragOver) // Dispara quando um item arrastado está sendo arrastado sobre um alvo de queda válido, repetidamente enquanto o item arrastado está dentro da zona de queda
-//   dropzone.addEventListener('dragleave', dragLeave) // Dispara quando um item é arrastado e colocado na caixa correta (?) ou quando o mouse sai da área dropável (?)
-//   dropzone.addEventListener('drop', drop) // Dispara quando um item é arrastado e solto (colocado) num destino válido
-// })
+document.addEventListener("dragstart", event => {
+  dragged = event.target;
+  document.getElementById("lembra-te").style.transform = "rotate(270deg)";
+  document.getElementById("neles").style.transform = "rotate(270deg)";
+  document.getElementById("dizer").style.transform = "rotate(270deg)";
+  document.getElementById("dias").style.transform = "rotate(270deg)";
+  document.getElementById("antes").style.transform = "rotate(270deg)";
+});
+
+document.addEventListener("dragover", event => {
+  event.preventDefault();
+});
+
+document.addEventListener("drop", event => {
+  // impedir a ação padrão (abrir como link para alguns elementos)
+  event.preventDefault();
+  //mover o elemento arrastado para o destino de soltar selecionado
+  if (event.target.className == "dropzone") {
+    dragged.parentNode.removeChild(dragged);
+    event.target.appendChild(dragged); 
+    if (Q26=="eclesiastes") {
+      this.dragable=false;
+    }
+  }
+});
+
+const Q1 = id="lembra-te"
+const Q2 = id="tambem"
+const Q3 = id="doteu"
+const Q25 = id="contentamento"
+const Q26 = id= "eclesiastes"
+
---
+++ scriptrandom.js
@@ -0,0 +1,9 @@
+embaralhaCards(){
+    const total = this.cards.length//length = extensão, duração, quantidade, lista
+    const cardEscolhido = Math.floor(Math.random() * total)
+    this.card[cardEscolhido].valor
+}
+
+const bolaDaVez = this.card.charAt(cardEscolhido)
+this.card[cardEscolhido].recebeZIndex(bolaDaVez)
+document.getElementsByClassName (cardEscolhido).style.ZIndex = []
